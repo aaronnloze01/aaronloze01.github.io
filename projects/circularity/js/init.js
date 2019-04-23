cdfa318288ae877303d653c4691303caab6d0ef9
Aaron Loze <aaronloze03@gmail.com>
2019-04-23 00:13:58 +0000

    fixing index.html

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,18 +20,29 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables //
-
+var circle;
+var circles = []
 
         // TODO 2 : Create a function that draws a circle  //
-        var drawCircle;
-        
+        var drawCircle = function(){
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
         // TODO 3 : Call the drawCircle function 5 times //
-
-
+                  
         // TODO 7 : Create a Loop to call drawCircle 100 times
-
-    
+    var counter = 0;
+    while (counter <101){
+      // do something
+      drawCircle();
+      counter++
+    }
+    for (var counter =0; counter <10; counter++){
+        // do something
+    }
         view.addChild(fps);
         app.addUpdateable(fps);
     
@@ -43,31 +54,35 @@ var init = function (window) {
                 circle.x = 0;
             } 
             // TODO 5a) if the circle has gone past of the LEFT side of the screen then place it on the RIGHT
-            else if ( / * Fill me in! * / ) {
-                
+            else if ( circle.x < 0){
+                circle.x = canvas.width;
             } 
 
             // TODO 5b) if the circle has gone past of the TOP side of the screen then place it on the BOTTOM
-            if ( / * Fill me in! * / ) {
-                
+            if ( circle.y > canvas.height) {
+            circle.y = 0;    
             }
             // TODO 5c) if the circle has gone past of the BOTTOM side of the screen then place it OFF-SCREEN TOP
-            else if ( / * Fill me in! * / ) {
-            
+            else if ( circle.y < canvas.height) {
+            circle.y = canvas.height;
             }
             // YOUR TODO 5 CODE ENDS HERE //////////////////////////
         }
     
         var update = function() {
             // TODO 4 : Update the circle's position //
-
+                // circles[ <index> ]
+                physikz.updatePosition(circles[0]);
             
             // TODO 5 : Call game.checkCirclePosition on your circles.
-           
-
+           game.checkCirclePosition(circles[0]);
             // TODO 8 : Iterate over the array
-           
-
+           var friends =['John', 'Max', 'George', 'Ben','Steve', 'Brian'];
+           var friend;
+           for (var i = 0; i < friends.length; i++){
+               friend =friends[i];
+               console.log(friend);
+           }
         }
         
         ////////////////////////////////////////////////////////////////////
